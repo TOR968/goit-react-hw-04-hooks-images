460a19409a3a5ecca1c5d98bb51bfe2b23ac85fa
TOR968 <tor968@yahoo.com>
2020-12-22 01:11:33 +0200

    Update image-api.js

--- my-app/src/components/utils/image-api.js
+++ my-app/src/components/utils/image-api.js
@@ -1,11 +1,11 @@
 import KEY from './key';
 import axios from 'axios';
 
-const get = (query, page = 1) => {
-  const response = axios.get(
+const get = async (query, page = 1) => {
+  const { data } = await axios.get(
     `https://pixabay.com/api/?q=${query}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=10`,
   );
-  return response;
+  return data.hits;
 };
 
 export default get;
