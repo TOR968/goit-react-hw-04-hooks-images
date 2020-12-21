cc02cc4ea29e36aebbe8ab109ff5bece788ff65b
TOR968 <tor968@yahoo.com>
2020-12-21 17:56:09 +0200

    Update GalleryContainer.js

--- my-app/src/components/GalleryContainer/GalleryContainer.js
+++ my-app/src/components/GalleryContainer/GalleryContainer.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import Searchbar from '../Components/Searchbar/Searchbar';
 import get from '../utils/image-api';
 import ImageGallery from '../Components/ImageGallery/ImageGallery';
@@ -6,93 +6,89 @@ import Button from '../Components/Button/Button';
 
 import LoaderSection from '../Components/Loader/Loader';
 
-class GalleryContainer extends Component {
-  state = {
-    gallery: [],
-    query: '',
-    page: 1,
-    isLoading: false,
-    error: false,
+function GalleryContainer() {
+  const [gallery, setGallery] = useState([]);
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const onChangeQuery = query => {
+    setQuery(query);
+    setPage(1);
+    setGallery([]);
   };
 
-  onChangeQuery = query => {
-    this.setState({
-      query,
-      page: 1,
-      gallery: [],
-    });
-  };
+  // useEffect(fetchRequest, []);
+  // useCallback(() => {
+  //   const fetchRequest = async () => {
+  //     try {
+  //       const response = await get(query, page);
+  //       return response.data.hits;
+  //     } catch (error) {
+  //       setError(true);
+  //     }
+  //   };
+  //   fetchRequest();
+  // }, []);
 
-  fetchRequest = async () => {
-    try {
-      const { query, page } = this.state;
-      const response = await get(query, page);
-      return response.data.hits;
-    } catch (error) {
-      this.setState({
-        error: true,
+
+  const fetchRequest = useCallback(() => {
+    async () => {
+      try {
+        const response = await get(query, page);
+        return response.data.hits;
+      } catch (error) {
+        setError(true);
+      }
+    }, [] );
+useEffect(() => {
+  fetchRequests()
+}, [fetchRequest])
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    const request = async () => {
+      const result = await fetchRequest();
+      setGallery([...result]);
+      setIsLoading(false);
+    };
+    request();
+  }, [query]);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    const request = async () => {
+      const result = await fetchRequest();
+      setGallery([...gallery, ...result]);
+      setIsLoading(false);
+
+      window.scrollTo({
+        top: document.documentElement.scrollHeight,
+        behavior: 'smooth',
       });
-    }
+    };
+    request();
+  }, [gallery, page]);
+
+  const pageChanger = () => {
+    setPage(prevPage => prevPage + 1);
   };
 
-  componentDidUpdate(prevProps, prevState) {
-    const { query, page } = this.state;
-
-    if (query !== prevState.query) {
-      this.setState({
-        isLoading: true,
-      });
-      const request = async () => {
-        const result = await this.fetchRequest();
-        this.setState({
-          gallery: [...result],
-          isLoading: false,
-        });
-      };
-      request();
-    }
-
-    if (page !== prevState.page) {
-      this.setState({
-        isLoading: true,
-      });
-      const request = async () => {
-        const result = await this.fetchRequest();
-        this.setState(state => ({
-          gallery: [...state.gallery, ...result],
-          isLoading: false,
-        }));
-
-        window.scrollTo({
-          top: document.documentElement.scrollHeight,
-          behavior: 'smooth',
-        });
-      };
-      request();
-    }
-  }
-
-  pageChanger = () => {
-    this.setState(state => ({
-      page: state.page + 1,
-    }));
-  };
-
-  render() {
-    const { gallery, isLoading, error } = this.state;
-    return (
-      <>
-        <Searchbar onChangeQuery={this.onChangeQuery} />
-        {error && <h1>something gone wrong, try again later</h1>}
-        {!!gallery.length && !error && (
-          <>
-            <ImageGallery gallery={gallery} />
-            {!isLoading && <Button pageChanger={this.pageChanger} />}
-          </>
-        )}
-        {isLoading && <LoaderSection />}
-      </>
-    );
-  }
+  return (
+    <>
+      <Searchbar onChangeQuery={onChangeQuery} />
+      {error && <h1>something gone wrong, try again later</h1>}
+      {!!gallery.length && !error && (
+        <>
+          <ImageGallery gallery={gallery} />
+          {!isLoading && <Button pageChanger={pageChanger} />}
+        </>
+      )}
+      {isLoading && <LoaderSection />}
+    </>
+  );
 }
 export default GalleryContainer;
